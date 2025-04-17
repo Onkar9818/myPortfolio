8da3632a117ee24058607353a60edc8608b83989
Onkar9818 <onkar32savaratkar@gmail.com>
2025-04-17 18:57:12 +0530

    Completed sections:Experience,Skills,Work Remaining:Testimonials

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import Hero from './sections/Hero'
-
+import NavBar from './components/HeroModels/NavBar'
+import ShowcaseSection from './sections/ShowcaseSection'
+import FeatureCards from './sections/FeatureCards'
+import ExperienceSection from './sections/ExperienceSection'
+import TechStack from './sections/TechStack'
+import Contact from './sections/Contact'
+import Footer from './sections/Footer'
 const App = () => {
   return (
     <>
+      <NavBar/>
       <Hero />
+      <ShowcaseSection/>
+      <FeatureCards/>
+      <ExperienceSection/>
+      <TechStack/>
+      <Contact />
+      <Footer />
     </>
   )
 }
--- src/constants/index.js
+++ src/constants/index.js
@@ -11,10 +11,10 @@ const navLinks = [
       name: "Skills",
       link: "#skills",
     },
-    {
-      name: "Testimonials",
-      link: "#testimonials",
-    },
+    // {
+    //   name: "Testimonials",
+    //   link: "#testimonials",
+    // },
   ];
   
   const words = [
@@ -147,39 +147,39 @@ const navLinks = [
   
   const expCards = [
     {
-      review: "Adrian brought creativity and technical expertise to the team, significantly improving our frontend performance. His work has been invaluable in delivering faster experiences.",
+      review: "Clothing E-commerce WebApp",
       imgPath: "/images/exp1.png",
-      logoPath: "/images/logo1.png",
+      logoPath: "/images/pythonlogo.png",
       title: "Frontend Developer",
-      date: "January 2023 - Present",
+      // date: "January 2023 - Present",
       responsibilities: [
-        "Developed and maintained user-facing features for the Hostinger website.",
+        "Developed and maintained user-facing features in Clothing website",
         "Collaborated closely with UI/UX designers to ensure seamless user experiences.",
         "Optimized web applications for maximum speed and scalability.",
       ],
     },
     {
-      review: "Adrian’s contributions to Docker's web applications have been outstanding. He approaches challenges with a problem-solving mindset.",
+      review: "Attendance System using face-recognition",
       imgPath: "/images/exp2.png",
-      logoPath: "/images/logo2.png",
+      logoPath: "/images/reactlogo.png",
       title: "Full Stack Developer",
-      date: "June 2020 - December 2023",
+      // date: "June 2020 - December 2023",
       responsibilities: [
-        "Led the development of Docker's web applications, focusing on scalability.",
+        "Led the development of Attendance web applications, focusing on scalability.",
         "Worked with backend engineers to integrate APIs seamlessly with the frontend.",
-        "Contributed to open-source projects that were used with the Docker ecosystem.",
+        "Contributed to open-source projects.",
       ],
     },
     {
-      review: "Adrian’s work on Appwrite’s mobile app brought a high level of quality and efficiency. He delivered solutions that enhanced our mobile experience & meet our product goals.",
+      review: "myPortfolio",
       imgPath: "/images/exp3.png",
-      logoPath: "/images/logo3.png",
-      title: "React Native Developer",
-      date: "March 2019 - May 2020",
+      logoPath: "/images/reactlogo.png",
+      title: "React Developer",
+      // date: "March 2019 - May 2020",
       responsibilities: [
-        "Built cross-platform mobile apps using React Native, integrating with Appwrite's backend services.",
+        "Built cross-platform mobile apps using React Native.",
         "Improved app performance and user experience through code optimization and testing.",
-        "Coordinated with the product team to implement features based on feedback.",
+        "Collaborated with designers to create visually appealing and user-friendly interfaces.",
       ],
     },
   ];
@@ -201,66 +201,69 @@ const navLinks = [
   
   const testimonials = [
     {
-      name: "Esther Howard",
-      mentions: "@estherhoward",
+      name: "",
+      mentions: "",
       review:
-        "I can’t say enough good things about Adrian. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.",
-      imgPath: "/images/client1.png",
+        "",
+      imgPath: "",
     },
     {
-      name: "Wade Warren",
-      mentions: "@wadewarren",
+      name: "",
+      mentions: "",
       review:
-        "Working with Adrian was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.",
-      imgPath: "/images/client3.png",
+        "",
+      imgPath: "",
     },
     {
-      name: "Guy Hawkins",
-      mentions: "@guyhawkins",
+      name: "",
+      mentions: "",
       review:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      imgPath: "/images/client2.png",
+        "",
+      imgPath: "",
     },
     {
-      name: "Marvin McKinney",
-      mentions: "@marvinmckinney",
+      name: "",
+      mentions: "",
       review:
-        "Adrian was a pleasure to work with. He turned our outdated website into a fresh, intuitive platform that’s both modern and easy to navigate. Fantastic work overall.",
-      imgPath: "/images/client5.png",
+        "",
+      imgPath: "",
     },
     {
-      name: "Floyd Miles",
-      mentions: "@floydmiles",
+      name: "",
+      mentions: "",
       review:
-        "Adrian’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional!",
-      imgPath: "/images/client4.png",
+        "",
+      imgPath: "",
     },
     {
-      name: "Albert Flores",
-      mentions: "@albertflores",
+      name: "",
+      mentions: "",
       review:
-        "Adrian was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend and backend dev are top-notch.",
-      imgPath: "/images/client6.png",
+        "",
+      imgPath: "",
     },
   ];
   
   const socialImgs = [
     {
-      name: "insta",
-      imgPath: "/images/insta.png",
+      name: "linkedin",
+      url:"https://www.linkedin.com/in/onkarsavaratkar/",
+      imgPath: "/images/linkedin.png",
+      
     },
     {
-      name: "fb",
-      imgPath: "/images/fb.png",
+      name: "insta",
+      url:"https://www.instagram.com/fitwithonkar/",
+      imgPath: "/images/insta.png",
+      
     },
     {
       name: "x",
+      url:"https://x.com/onkar_21?s=11",
       imgPath: "/images/x.png",
+      
     },
-    {
-      name: "linkedin",
-      imgPath: "/images/linkedin.png",
-    },
+    
   ];
   
   export {
